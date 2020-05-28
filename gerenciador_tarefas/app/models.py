b96dedfeb70b66ba899d51d6494ccae749c03516
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-05-28 19:04:35 -0300

    Criando a migration

--- gerenciador_tarefas/app/models.py
+++ gerenciador_tarefas/app/models.py
@@ -5,10 +5,11 @@ from django.db import models
 class Tarefa(models.Model):
     PRIORIDADE_CHOICES = [
         ('A', 'Alta'),
-        ('M', 'Media'),
+        ('N', 'Normal'),
         ('B', 'Baixa')
     ]
     titulo = models.CharField(max_length = 30, null = False, blank = False)
     descricao = models.CharField(max_length = 100, null = False, blank = False)
     data_expiracao = models.DateField(null = False, blank = False),
-    prioridade =
+    prioridade = models.CharField(max_length = 1, choices = PRIORIDADE_CHOICES, null = False, blank = False)
+
