71d510b0467e0fed71482a0f462193b66b1591b6
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-05-28 19:34:35 -0300

    Adicionando os campos ao formulario

--- gerenciador_tarefas/app/urls.py
+++ gerenciador_tarefas/app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('listar_tarefas/', listar_tarefas, name = 'listar_tarefas')
+    path('listar_tarefas/', listar_tarefas, name = 'listar_tarefas'),
+    path('cadastrar_tarefa/', cadastrar_tarefa, name = 'cadastrar_tarefa')
 ]
--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .forms import TarefaForm
 
 # Create your views here.
 
 def listar_tarefas(request):
     nome_tarefa = 'Assistir a Semana Python & Django'
     return render(request, 'tarefas/listar_tarefas.html', {'nome_tarefa': nome_tarefa})
+
+def cadastrar_tarefa(request):
+    form_tarefa = TarefaForm()
+    return render(request, 'tarefas/form_tarefa.html', {'form_tarefa': form_tarefa})
