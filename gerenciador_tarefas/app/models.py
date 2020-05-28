4c8ed034c398a6840fe482fb6f7926b3eabb3ce0
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-05-28 19:00:29 -0300

    Criando o model para a migration

--- gerenciador_tarefas/app/models.py
+++ gerenciador_tarefas/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarefa(models.Model):
+    PRIORIDADE_CHOICES = [
+        ('A', 'Alta'),
+        ('M', 'Media'),
+        ('B', 'Baixa')
+    ]
+    titulo = models.CharField(max_length = 30, null = False, blank = False)
+    descricao = models.CharField(max_length = 100, null = False, blank = False)
+    data_expiracao = models.DateField(null = False, blank = False),
+    prioridade =
