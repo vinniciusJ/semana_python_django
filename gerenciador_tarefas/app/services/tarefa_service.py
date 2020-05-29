e91dc56f3752808d67b4ee938b347f475225bfce
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-05-28 22:32:29 -0300

    CRUD finalizado

--- gerenciador_tarefas/app/services/tarefa_service.py
+++ gerenciador_tarefas/app/services/tarefa_service.py
@@ -10,3 +10,18 @@ def cadastrar_tarefa(tarefa):
 
 def listar_tarefas():
     return Tarefa.objects.all()
+
+def listar_tarefa_id(id):
+    return Tarefa.objects.get(id = id)
+
+def editar_tarefa(tarefa_bd, tarefa_nova):
+    tarefa_bd.titulo = tarefa_nova.titulo
+    tarefa_bd.descricao = tarefa_nova.descricao
+    tarefa_bd.data_expiracao = tarefa_nova.data_expiracao
+    tarefa_bd.prioridade = tarefa_nova.prioridade
+
+    tarefa_bd.save(force_update = True)
+
+def remover_tarefa(tarefa_bd):
+    tarefa_bd.delete()
+
--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -31,3 +31,33 @@ def cadastrar_tarefa(request):
         form_tarefa = TarefaForm()
 
     return render(request, 'tarefas/form_tarefa.html', {'form_tarefa': form_tarefa})
+
+def editar_tarefa(request, id):
+    tarefa_bd = tarefa_service.listar_tarefa_id(id)
+    form_tarefa = TarefaForm(request.POST or None, instance = tarefa_bd)
+
+    if form_tarefa.is_valid():
+        titulo = form_tarefa.cleaned_data['titulo']
+        descricao = form_tarefa.cleaned_data['descricao']
+        data_expiracao = form_tarefa.cleaned_data['data_expiracao']
+        prioridade = form_tarefa.cleaned_data['prioridade']
+
+        print(titulo)
+
+        tarefa_nova = Tarefa(titulo=titulo, descricao=descricao, data_expiracao=data_expiracao, prioridade=prioridade)
+        print(tarefa_nova.titulo)
+
+        tarefa_service.editar_tarefa(tarefa_bd, tarefa_nova)
+
+        return redirect('listar_tarefas')
+
+    return render(request, 'tarefas/form_tarefa.html', {'form_tarefa': form_tarefa})
+
+def remover_tarefa(request, id):
+    tarefa_bd = tarefa_service.listar_tarefa_id(id)
+    if request.method == 'POST':
+        tarefa_service.remover_tarefa(tarefa_bd)
+
+        return redirect('listar_tarefas')
+
+    return render(request, 'tarefas/confirma_exclusao.html', {'tarefa': tarefa_bd})
